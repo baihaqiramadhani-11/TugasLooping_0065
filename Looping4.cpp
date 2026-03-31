5c67c7c1f143d129b3df6576fd92487dc604f41d
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-31 15:01:59 +0700

    function header

--- Looping4.cpp
+++ Looping4.cpp
@@ -6,3 +6,12 @@ using namespace std;
 // =============================================
 int angka = 0;
 int pilihan = 0;
+
+// =============================================
+// FUNCTION PENGECEKAN
+// =============================================
+
+// Function untuk mengecek bilangan prima menggunakan while loop
+bool isPrima(int n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 3) return true;
