21f8be27a89ebd432708ce67394dcd422a520dfa
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-31 15:01:11 +0700

    global variables

--- Looping4.cpp
+++ Looping4.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
 using namespace std;
+
+// =============================================
+// VARIABEL GLOBAL
+// =============================================
+int angka = 0;
+int pilihan = 0;
