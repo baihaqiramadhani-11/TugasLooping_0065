eff180793a2bdff891c764fa0cb48f24f93c69af
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-31 15:02:40 +0700

    correct formatting

--- Looping4.cpp
+++ Looping4.cpp
@@ -15,3 +15,13 @@ int pilihan = 0;
 bool isPrima(int n) {
     if (n <= 1) return false;
     if (n == 2 || n == 3) return true;
+
+     int i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
