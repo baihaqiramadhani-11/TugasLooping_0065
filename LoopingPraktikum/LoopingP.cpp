e7707ffce1d03e87e076cd29ee9a4050ee833ff2
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-03-31 16:13:38 +0700

    int

--- LoopingPraktikum/LoopingP.cpp
+++ LoopingPraktikum/LoopingP.cpp
@@ -8,3 +8,13 @@ int pilihan;
 // ===== Function Cek Prima =====
 bool isPrima(int n) {
     if (n <= 1) return false;
+
+    int i = 2;
+    while (i <= n / 2) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
